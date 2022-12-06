dc57e25a65cb89ae078e8c17570aed22aba853ec
cyoyoyoa <cyoyoyoa@gmail.com>
2022-12-06 05:46:55 +0000

    FBV 대문페이지

--- single_pages/views.py
+++ single_pages/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+
+# 함수방식( <-> class)
+def landing(request) :
+    return render(
+        request, 
+        'single_pages/landing.html'
+    )
+def about_me(request) :
+    return render(
+        request,
+        'single_pages/about_me.html'
+    )
+
 # Create your views here.
