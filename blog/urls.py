e689eda2ba2d20a8cf895bc5eaa1849f3c909641
cyoyoyoa <cyoyoyoa@gmail.com>
2022-12-06 07:05:58 +0000

    CBV Detail page update

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('<int:pk>/', views.single_post_page),  # blog/1  , single_post_page-로 가겠다   
-    path('', views.index), 
+    path('<int:pk>/', views.PostDetail.as_view()),
+#   path('<int:pk>/', views.single_post_page),  # blog/1  , single_post_page-로 가겠다   
+#   path('', views.index),  
+    path('',views.PostList.as_view()),
+   # post_list.html 
+    
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,46 @@
-from django.shortcuts import render
-from .models import Post 
+# class 방식
+
+# from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
 
-def index(request):  
-    posts = Post.objects.all().order_by('-pk') 
+class PostList(ListView) :  # 아래 index부분과 같음.
+    model = Post  # model 우리가 정한 함수  
+    ordering = '-pk'
     
-    return render (
-        request, 
-        'blog/index.html',
-        {
-            'posts':posts,
-        }
-    )
+   # template_name = 'blog/index.html'  # 이렇게도 가능
+    
+class PostDetail(DetailView):
+    model = Post
+    
 
-def single_post_page(request, pk) : 
-    post = Post.objects.get(pk=pk)  
     
-    return render(
-        request,
-        'blog/single_post_page.html', 
-        {
-             'post' : post,    
-        }
-    )
+# 함수방식 
+# from django.shortcuts import render
+# from .models import Post 
+
+
+# def index(request):  
+#     posts = Post.objects.all().order_by('-pk') 
     
-# Create your views here.
+#     return render (
+#         request, 
+#         'blog/index.html',
+#         {
+#             'posts':posts,
+#         }
+#     )
+
+# def single_post_page(request, pk) : 
+#     post = Post.objects.get(pk=pk)  
+    
+#     return render(
+#         request,
+#         'blog/single_post_page.html', 
+#         {
+#              'post' : post,    
+#         }
+#     )
+    
+# # Create your views here.
