caf46435a18716ed20f47b524ee98d96db860c4a
cyoyoyoa <cyoyoyoa@gmail.com>
2022-12-12 01:31:35 +0000

    category

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,10 @@
-from django.urls import path 
-from . import views 
+from django.urls import path
+from . import views
 
 urlpatterns = [
+    path('category/<str:slug>/', views.category_page),
     path('<int:pk>/', views.PostDetail.as_view()),
-#   path('<int:pk>/', views.single_post_page),  # blog/1  , single_post_page-로 가겠다   
-#   path('', views.index),  
-    path('',views.PostList.as_view()),
-   # post_list.html 
-    
+    path('', views.PostList.as_view()),
+    # path('<int:pk>/', views.single_post_page),
+    # path('', views.index),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,46 +1,68 @@
-# class 방식
-
-# from django.shortcuts import render
+from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Post
+from .models import Post, Category
 
 
-class PostList(ListView) :  # 아래 index부분과 같음.
-    model = Post  # model 우리가 정한 함수  
+class PostList(ListView):
+    model = Post
     ordering = '-pk'
-    
-   # template_name = 'blog/index.html'  # 이렇게도 가능
-    
+
+    def get_context_data(self, **kwargs):
+        context = super(PostList, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        return context
+
+
 class PostDetail(DetailView):
     model = Post
     
+    def get_context_data(self, **kwargs):
+        context = super(PostDetail, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        return context
 
-    
-# 함수방식 
-# from django.shortcuts import render
-# from .models import Post 
+
+def category_page(request, slug):
+    if slug == 'no_category':
+        category = '미분류'
+        post_list = Post.objects.filter(category=None)
+    else:
+        category = Category.objects.get(slug=slug)
+        post_list = Post.objects.filter(category=category)
+
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list': post_list,
+            'categories': Category.objects.all(),
+            'no_category_post_count': Post.objects.filter(category=None).count(),
+            'category': category,
+        }
+    )
 
 
-# def index(request):  
-#     posts = Post.objects.all().order_by('-pk') 
-    
-#     return render (
-#         request, 
-#         'blog/index.html',
-#         {
-#             'posts':posts,
-#         }
-#     )
-
-# def single_post_page(request, pk) : 
-#     post = Post.objects.get(pk=pk)  
-    
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk')
+#
 #     return render(
 #         request,
-#         'blog/single_post_page.html', 
+#         'blog/index.html',
 #         {
-#              'post' : post,    
+#             'posts': posts,
 #         }
 #     )
-    
-# # Create your views here.
+
+
+# def single_post_page(request, pk):
+#     post = Post.objects.get(pk=pk)
+#
+#     return render(
+#         request,
+#         'blog/single_post_page.html',
+#         {
+#             'post': post,
+#         }
+#     )
