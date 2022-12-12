8d0ccf832d3f45e6efa0034472c9cc5e47fd5e4d
cyoyoyoa <cyoyoyoa@gmail.com>
2022-12-12 06:23:29 +0000

    list -detail tag

--- blog/models.py
+++ blog/models.py
@@ -16,6 +16,17 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = 'categories'
+        
+        
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/blog/category/{self.slug}/'
 
 
 class Post(models.Model):
@@ -33,6 +44,10 @@ class Post(models.Model):
 
     category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
 
+    tags = models.ManyToManyField(Tag, null=True, blank=True)
+    
+
+    
     def __str__(self):
         return f'[{self.pk}]{self.title} :: {self.author}'
 
@@ -44,3 +59,5 @@ class Post(models.Model):
 
     def get_file_ext(self):
         return self.get_file_name().split('.')[-1]
+
+    
